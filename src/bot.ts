ee711f249caeeb40e53e30c433af82fa8c1d2aca
Douglass Hooker <DouglassHooker@gmail.com>
2021-01-12 16:25:06 -0600

    Update server to use GoatBot singleton

--- src/bot.ts
+++ src/bot.ts
@@ -5,11 +5,77 @@ import { rateHandler } from './commands/rate'
 import { remindMeHandler } from './commands/remind-me'
 import { voteHandler } from './commands/vote'
 
-const client = new Client()
+export class GoatBot {
+  public client: Client | null = null
+  public token = ''
 
-client.on('ready', () => {
-  console.info(`Client ready, logged in as ${client.user?.tag}`)
-})
+  public init(): void {
+    this.client = new Client()
+  }
+
+  public async login(): Promise<void> {
+    if (!process.env.BOT_TOKEN) {
+      throw new Error('Cannot run bot without bot token')
+    }
+    const token = await this.client?.login(process.env.BOT_TOKEN)
+    if (!token) {
+      console.error('[Goat Bot] Could not login - check logs')
+    }
+  }
+  // Attach all event listeners
+  public setup(): void {
+    // Do any high level connection stuff here, including listeners
+    this.client?.on('message', this.onMessage)
+    this.client?.on('ready', this.onReady)
+  }
+
+  public destroy(): void {
+    console.info('Deactivating bot')
+    this.client?.destroy()
+    this.client = null
+  }
+
+  public onMessage(msg: Message): void {
+    try {
+      // goat-bot ignores himself and other bots
+      if (
+        msg.member?.user.username === 'goat-bot' ||
+        msg.author.bot ||
+        !msg.content.startsWith('!')
+      ) {
+        return
+      }
+
+      BotCommandHandlers[matchCommand(msg) || 'help'](msg)
+    } catch (e) {
+      console.error(e)
+      msg.reply('I am Error. Check the logs, buddy')
+    }
+  }
+
+  public onReady(): void {
+    console.info(`Client ready, logged in as ${this.client?.user?.tag}`)
+    console.log('[Goat Bot] I am awake!')
+  }
+
+  public async activate(): Promise<void> {
+    this.init()
+    this.setup()
+    await this.login()
+  }
+
+  public deactivate(): void {
+    this.destroy()
+  }
+}
+
+export interface ClientAsJSON {
+  shard: null
+  users: string[]
+  guilds: string[]
+  channels: string[]
+  user: string | null
+}
 
 export const BotCommands = {
   help: '!help',
@@ -41,43 +107,3 @@ const matchCommand = (msg: Message): BotCommand | null => {
 
   return null
 }
-
-client.on('message', (msg) => {
-  try {
-    // goat-bot ignores himself and other bots
-    if (
-      msg.member?.user.username === 'goat-bot' ||
-      msg.author.bot ||
-      !msg.content.startsWith('!')
-    ) {
-      return
-    }
-
-    BotCommandHandlers[matchCommand(msg) || 'help'](msg)
-  } catch (e) {
-    console.error(e)
-    msg.reply('I am Error. Check the logs, buddy')
-  }
-})
-
-// Do any high level connection stuff here
-client.on('ready', () => {
-  console.log('I am awake!')
-})
-
-// Logging in
-export const activateBot = async (): Promise<string> => {
-  if (!process.env.BOT_TOKEN) {
-    throw new Error('Cannot run bot without bot token')
-  }
-
-  console.info('Activating bot')
-  return await client.login(process.env.BOT_TOKEN)
-}
-
-export const deactivateBot = (): void => {
-  console.info('Deactivating bot')
-  client.destroy()
-}
-
-export default client
--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,90 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
-import client, { activateBot, deactivateBot } from './bot'
+import { ClientAsJSON, GoatBot } from './bot'
 
 const app = express()
 app.use(express.json())
+
+// Use a singleton to simulate a global state
+const bot = new GoatBot()
+
+// app.use((req, res, next) => {
+//   const provided = req.headers.authorization,
+//     expected = process.env.NODE_AUTH_KEY
+
+//   if (!expected) {
+//     throw new Error('NODE_AUTH_KEY not found in env')
+//   }
+
+//   if (provided === expected) {
+//     next()
+//   } else {
+//     res.send(403).json({
+//       message: 'Please provide the expected API token',
+//     })
+//   }
+// })
 
 app.get('/health', (req, res) => {
   console.log('Health check')
 
   res.status(200).json({
-    client: client.toJSON(),
+    client: bot.client?.toJSON(),
   })
 })
 
 app.post('/start', async (req, res) => {
   console.info('[ Signal ] - Start')
 
-  const status = await activateBot()
-  res.status(200).json({
-    status,
-    client: client.toJSON(),
-  })
+  const c = bot.client?.toJSON() as ClientAsJSON | undefined
+  console.log(c)
+  if (!c?.user) {
+    const status = await bot.activate()
+    res.status(200).json({
+      status,
+      client: c,
+    })
+  } else {
+    res.status(409).json({
+      message: 'Will not activate bot, as bot is already running',
+    })
+  }
 })
 
 app.post('/stop', (req, res) => {
   console.info('[ Signal ] - Stop')
-  deactivateBot()
 
-  res.status(200).end()
+  const c = bot.client?.toJSON() as ClientAsJSON | undefined
+  if (c && c.user) {
+    bot.destroy()
+    res.status(200).end()
+  } else {
+    res.status(409).json({
+      message: 'Will not deactivate bot, as bot is already deactivated',
+    })
+  }
+})
+
+app.get('/bot-status', (req, res) => {
+  if (bot.client) {
+    res.status(200).json({
+      status: 'active',
+    })
+  } else {
+    res.status(200).json({
+      status: 'inactive',
+    })
+  }
 })
 
 app.post('/restart', async (req, res) => {
   console.info('[ Signal ] - Restart')
-  deactivateBot()
+  bot.destroy()
 
-  const status = await activateBot()
+  const status = await bot.activate()
   res.json({
     status,
-    client: client.toJSON(),
+    client: bot.client?.toJSON(),
   })
 })
 
@@ -48,5 +95,5 @@ app.listen(8000, '0.0.0.0', async () => {
   dotenv.config()
 
   // Bot initial setup, acts as the first "activate" call
-  await activateBot()
+  await bot.activate()
 })
