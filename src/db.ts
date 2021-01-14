089220eeccd3e886b91deab8e32302a555f51977
Douglass Hooker <DouglassHooker@gmail.com>
2021-01-13 20:13:01 -0600

    Add firstore db

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import { Firestore } from '@google-cloud/firestore'
+
+export const dbConnection = (): Firestore => {
+  dotenv.config()
+  console.log(process.env.GCP_PROJECT_ID && process.env.GCP_PROJECT_KEYFILE)
+  if (process.env.GCP_PROJECT_ID && process.env.GCP_PROJECT_KEYFILE) {
+    return new Firestore({
+      projectId: process.env.GCP_PROJECT_ID,
+      keyFilename: process.env.GCP_PROJECT_KEYFILE,
+    })
+  } else {
+    throw new Error(
+      'Could not find GCP_PROJECT_ID or GCP_PROJECT_KEYFILE in env',
+    )
+  }
+}
